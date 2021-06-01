9bb5a4c547595b9783a2a3147d61c35c66dc5856
Vasilis Polynopoulos <62022541+OxymoroVP@users.noreply.github.com>
2021-06-01 18:39:38 +0300

    Add files via upload

---
+++ python-flask-server/swagger_server/models/__init__.py
@@ -0,0 +1,11 @@
+# coding: utf-8
+
+# flake8: noqa
+from __future__ import absolute_import
+# import models into model package
+from swagger_server.models.appointment import Appointment
+from swagger_server.models.calendar import Calendar
+from swagger_server.models.medical_folder import MedicalFolder
+from swagger_server.models.patient import Patient
+from swagger_server.models.patient_appointment import PatientAppointment
+from swagger_server.models.request import Request
